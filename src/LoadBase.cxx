15ccb38099890ac00a65466ecf910d5cbf79d855
Karol Krizka <kkrizka@gmail.com>
2017-05-30 23:12:01 -0700

    Start of testing framework.

---
+++ src/LoadBase.cxx
@@ -0,0 +1,93 @@
+#include "LoadBase.h"
+
+#include <sstream>
+#include <ctime>
+#include <iostream>
+
+LoadBase::LoadBase()
+  : m_fh(0), m_tree(0),
+    m_nBranches(0)
+{
+  for(uint i=0;i<50;i++)
+    m_pointers[i]=new std::vector<float>();
+}
+
+LoadBase::~LoadBase()
+{
+  for(uint i=0;i<50;i++)
+    {
+      delete m_pointers[i];
+      m_pointers[i]=0;
+    }
+}
+
+void LoadBase::setPath(const std::string& path)
+{ m_path=path; }
+
+void LoadBase::setNBranches(uint nBranches)
+{ m_nBranches=nBranches; }
+
+void LoadBase::setNHists(uint nHists)
+{ m_nHists=nHists; }
+
+void LoadBase::setupInput()
+{
+  if(m_fh!=0) m_fh->Close();
+  m_fh=TFile::Open(m_path.c_str());
+  m_tree=dynamic_cast<TTree*>(m_fh->Get("tree"));
+}
+
+void LoadBase::setupBranches()
+{
+  m_tree->SetBranchStatus("*", 0);
+  
+  std::stringstream ss;
+  for(uint i=0;i<m_nBranches;i++)
+    {
+      ss.str("");
+      ss << "test" << std::setfill('0') << std::setw(2) << i;
+      m_tree->SetBranchStatus(ss.str().c_str(), 1);
+      m_tree->SetBranchAddress(ss.str().c_str(), &m_pointers[i]);
+    }  
+}
+
+void LoadBase::setupHists()
+{
+  //for(auto hist : m_hists)
+  //delete hist;
+  m_hists.clear();
+
+  std::stringstream ss;  
+  for(uint i=0;i<m_nHists;i++)
+    {
+      ss.str("");
+      ss << "hist" << std::setfill('0') << std::setw(10) << i;
+      m_hists.push_back(new TH1F(ss.str().c_str(),"",10,-0.5,9.5));
+    }
+
+}
+
+double LoadBase::eventSize() const
+{
+  return m_fh->GetSize()/m_tree->GetEntries();
+}
+
+double LoadBase::runTest()
+{
+  setupInput();
+  setupBranches();
+  setupHists();
+
+  std::clock_t tbegin = std::clock();
+  for(uint i=0;i<m_tree->GetEntries();i++)
+    {
+      m_tree->GetEntry(i);
+      for(uint hidx=0;hidx<m_hists.size();hidx++)
+	m_hists[hidx]->Fill(m_pointers[hidx%m_nBranches]->at(0));
+    }
+  std::clock_t tend = std::clock(); 
+  double elapsed_secs = double(tend - tbegin) / CLOCKS_PER_SEC;
+  //std::cout << "time " << elapsed_secs << std::endl;
+  //std::cout << "rate " << m_tree->GetEntries()/elapsed_secs << std::endl;
+  return elapsed_secs/m_tree->GetEntries();
+}
---
+++ src/LoadBase.h
@@ -0,0 +1,42 @@
+#ifndef LOADBASE_H_
+#define LOADBASE_H_
+
+#include <vector>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TH1.h>
+
+class LoadBase
+{
+public:
+  LoadBase();
+  ~LoadBase();
+
+  void setPath(const std::string& path);
+  void setNBranches(uint nBranches);
+  void setNHists(uint nHists);
+
+  double eventSize() const;
+
+  double runTest();
+
+protected:
+  void setupInput();
+  void setupBranches();
+  void setupHists();
+
+private:
+  std::string m_path;
+  uint m_nBranches;
+  uint m_nHists;
+  
+  TFile *m_fh;
+  TTree *m_tree;
+
+  std::vector<float>* m_pointers[50];
+  std::vector<TH1*> m_hists;
+};
+
+#endif // LOADBASE_H_
