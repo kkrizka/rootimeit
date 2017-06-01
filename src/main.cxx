f6f2698ee2b7a7192dca4edbcefc5da7a4660969
Karol Krizka <kkrizka@gmail.com>
2017-05-31 23:48:50 -0700

    Run all variations in main.

--- src/main.cxx
+++ src/main.cxx
@@ -2,12 +2,28 @@
 
 #include "LoadBase.h"
 #include "LoadVector.h"
+#include "LoadArray.h"
+#include "LoadTClonesArray.h"
 
 int main(int argc, char* argv[])
 {
-  LoadBase *lb=new LoadVector();
+  LoadBase *lb=0;
+
+  lb=new LoadVector();
   lb->setPath("/ssdisk02/rootimeit/test_nbr00050_comp0.root");
   lb->setNBranches(50);
   lb->setNHists(0);
-  std::cout << lb->runTest() << std::endl;
+  std::cout << "vector " << lb->runTest() << std::endl;
+
+  lb=new LoadArray();
+  lb->setPath("/ssdisk02/rootimeit/test_array_nbr00050_comp0.root");
+  lb->setNBranches(50);
+  lb->setNHists(0);
+  std::cout << "array " << lb->runTest() << std::endl;
+
+  lb=new LoadTClonesArray();
+  lb->setPath("/ssdisk02/rootimeit/test_tclonesarray_nbr00050_comp0.root");
+  lb->setNBranches(50);
+  lb->setNHists(0);
+  std::cout << "TClonesArray " << lb->runTest() << std::endl;
 }
